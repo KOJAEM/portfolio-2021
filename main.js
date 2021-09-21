c6b684a93697a17c11ffa8ac56becdaa617e4445
KoJaem <woalswhwh@naver.com>
2021-09-21 22:05:17 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -28,9 +28,26 @@ contactMe.addEventListener('click', () => {
     scrollIntoView('#contact');
 })
 
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height; // homeHeight = 762
 
-// scrollMove Event
+document.addEventListener('scroll', () => {
+    if(window.scrollY >= 762) return;
+
+    home.style.opacity = 1-window.scrollY/homeHeight;
+})
+
+
+
+
+
+
+
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
 }
+
